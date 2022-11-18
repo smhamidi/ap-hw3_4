ffb4ef3940153431a1ce23a6ba4224402d15a3b8
smhamidi <1380hamidi@gmail.com>
2022-11-18 16:31:57 +0330

    Adding constructors and getter functions

--- include/message.h
+++ include/message.h
@@ -1,4 +1,24 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
+#include <chrono>
+#include <ctime>
+#include <string>
 
-#endif //MESSAGE_H
+class Message {
+public:
+  Message(std::string _type, std::string _sender, std::string _receiver);
+  Message();
+
+  std::string get_type();
+  std::string get_sender();
+  std::string get_receiver();
+  std::string get_time();
+
+private:
+  std::string type;     // type of the message ("text", "voice", ...)
+  std::string sender;   // the username who send this message
+  std::string receiver; // the username whom this message is intended for
+  std::string time;     // creation time of the message
+};
+
+#endif // MESSAGE_H
--- src/message.cpp
+++ src/message.cpp
@@ -1,1 +1,17 @@
 #include "message.h"
+#include "../include/message.h"
+
+Message::Message(std::string _type, std::string _sender, std::string _receiver)
+    : type{_type}, sender{_sender}, receiver{_receiver} {
+  std::time_t now = std::time(0);
+  std::string _time = std::ctime(&now);
+  _time.erase(_time.find_last_not_of(" \n\r\t") + 1);
+  time = _time;
+}
+
+Message::Message() : Message::Message("", "", "") {}
+
+std::string Message::get_type() { return type; }
+std::string Message::get_sender() { return sender; }
+std::string Message::get_receiver() { return receiver; }
+std::string Message::get_time() { return time; }
