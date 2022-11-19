3e8258023b26cb8c3fe3dd26f488db2d35d04dfd
smhamidi <1380hamidi@gmail.com>
2022-11-19 19:25:09 +0330

    Got the job done for text & voice classes

--- include/message.h
+++ include/message.h
@@ -2,17 +2,25 @@
 #define MESSAGE_H
 #include <chrono>
 #include <ctime>
+#include <iostream>
 #include <string>
+#include <vector>
 
 class Message {
 public:
   Message(std::string _type, std::string _sender, std::string _receiver);
   Message();
 
-  std::string get_type();
-  std::string get_sender();
-  std::string get_receiver();
-  std::string get_time();
+  std::string get_type() const;
+  std::string get_sender() const;
+  std::string get_receiver() const;
+  std::string get_time() const;
+
+  virtual void print(std::ostream &os) const;
+  friend std::ostream &operator<<(std::ostream &os, const Message &c) {
+    c.print(os);
+    return os;
+  };
 
 private:
   std::string type;     // type of the message ("text", "voice", ...)
@@ -21,4 +29,23 @@ private:
   std::string time;     // creation time of the message
 };
 
+class TextMessage : public Message {
+public:
+  TextMessage(std::string text, std::string sender, std::string receiver);
+  void print(std::ostream &os) const;
+  std::string get_text() const { return text; }
+
+private:
+  std::string text;
+};
+
+class VoiceMessage : public Message {
+public:
+  VoiceMessage(std::string sender, std::string receiver);
+  void print(std::ostream &os) const;
+  std::vector<unsigned char> get_voice() const { return voice; }
+
+private:
+  std::vector<unsigned char> voice;
+};
 #endif // MESSAGE_H
--- src/message.cpp
+++ src/message.cpp
@@ -11,7 +11,51 @@ Message::Message(std::string _type, std::string _sender, std::string _receiver)
 
 Message::Message() : Message::Message("", "", "") {}
 
-std::string Message::get_type() { return type; }
-std::string Message::get_sender() { return sender; }
-std::string Message::get_receiver() { return receiver; }
-std::string Message::get_time() { return time; }
+std::string Message::get_type() const { return type; }
+std::string Message::get_sender() const { return sender; }
+std::string Message::get_receiver() const { return receiver; }
+std::string Message::get_time() const { return time; }
+
+void Message::print(std::ostream &os) const {
+  std::string stars(30, '*');
+  os << stars << std::endl;
+  os << sender << " -> " << receiver << std::endl;
+  os << "message type: " << type << std::endl;
+  os << "message time: " << time << std::endl;
+  os << stars << std::endl;
+}
+
+TextMessage::TextMessage(std::string _text, std::string _sender,
+                         std::string _receiver)
+    : Message("text", _sender, _receiver), text{_text} {}
+
+void TextMessage::print(std::ostream &os) const {
+  std::string stars(30, '*');
+  os << stars << std::endl;
+  os << get_sender() << " -> " << get_receiver() << std::endl;
+  os << "message type: " << get_type() << std::endl;
+  os << "message time: " << get_time() << std::endl;
+  os << "text: " << get_text() << std::endl;
+  os << stars << std::endl;
+}
+
+VoiceMessage::VoiceMessage(std::string _sender, std::string _receiver)
+    : Message("voice", _sender, _receiver) {
+  for (size_t i = 0; i < 5; i++) {
+    voice.push_back(i);
+  }
+}
+void VoiceMessage::print(std::ostream &os) const {
+  std::string stars(30, '*');
+  os << stars << std::endl;
+  os << get_sender() << " -> " << get_receiver() << std::endl;
+  os << "message type: " << get_type() << std::endl;
+  os << "message time: " << get_time() << std::endl;
+  os << "voice: ";
+  for (auto i : get_voice()) {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+
+  os << stars << std::endl;
+}
